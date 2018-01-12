e21a06e033d9af709c71307c415ce055ef0b9518
Viliam Serecun <v.serecun@gmail.com>
2018-01-13 00:42:45 +0100

    src: added custom exception.
    NOTE: I need to reevaluate this exception. It depends on logging.
    Signed-off-by: Viliam Serecun <v.serecun@gmail.com>
    
    (cherry picked from commit af0a8c8)
    Signed-off-by: Viliam Serecun <v.serecun@gmail.com>

--- src/lemmiwinks/parser/exception.py
+++ src/lemmiwinks/parser/exception.py
@@ -1,3 +1,14 @@
 class ParserError(Exception):
     pass
 
+
+class CSSParseError(ParserError):
+    pass
+
+
+class HTMLParseError(ParserError):
+    pass
+
+
+class TagDoesNotExists(HTMLParseError):
+    pass
