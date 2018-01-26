ff545f49af623c26970e78ae4152525befb3f976
Viliam Serecun <v.serecun@gmail.com>
2018-01-26 16:46:32 +0100

    mod: created new methods for filter encapsulation.
    This method encapsulate filter rules of searched elements.
    
    Signed-off-by: Viliam Serecun <v.serecun@gmail.com>

--- lemmiwinks/archive/migrate/container.py
+++ lemmiwinks/archive/migrate/container.py
@@ -1,77 +1,28 @@
-import os.path
-import mimetypes
-from typing import Callable
+class ElementFilter:
+    _element_sources = {"img": [{"src": True}, {"data-src": True}],
+                        "video": [{"src": True}, {"poster": True}],
+                        "embed": [{"src": True}],
+                        "source": [{"src": True}],
+                        "audio": [{"src": True}],
+                        "input": [{"src": True}],
+                        "object": [{"data": True}, {"codebase": True}],
+                        "track": [{"src": True}]}
+    _link_stylesheet = {"link": [{"href": True, "rel": "stylesheet"}]}
+    _js_style = {"script": [{"src": True}]}
 
-# third party import
-import magic
+    @property
+    def element(self):
+        return self.__flatten(ElementFilter._element_sources)
 
-import parser.container
-from http_client.provider import HTTPClientDownloader
-import http_client.resolver as resolver
-from . import migrate
+    @property
+    def links_stylesheets(self):
+        return self.__flatten(ElementFilter._link_stylesheet)
 
-
-class HTMLElementFilters:
-    element_filter = {"img": ["src", "data-src"],
-                      "video": ["src", "poster"],
-                      "embed": ["src"],
-                      "source": ["src"],
-                      "audio": ["src"],
-                      "input": ["src"],
-                      "object": ["data", "codebase"],
-                      "track": ["src"]}
-    link_filter = {"link": ["href"]}
-    js_filter = {"script": ["src"]}
-
-
-class WebPageMigration:
-    def __init__(self,
-                 dst: str,
-                 file: object,
-                 url: str,
-                 filepath_gen: Callable,
-                 http_downloader: HTTPClientDownloader,
-                 html_parser: Callable):
-        self._index_path = self.__get_index_filepath(file.name, dst)
-        self._index_files_path = os.path.join(dst, "index_files")
-        self._html_parser = html_parser(file)
-        self._http_downloader = http_downloader
-        self._filepath_gen = filepath_gen(self._index_files_path, "index_files")
-        self._resolver = resolver.URLResolver(url)
+    @property
+    def js_style(self):
+        return self.__flatten(ElementFilter._js_style)
 
     @staticmethod
-    def __get_index_filepath(filename, dst):
-        mime = magic.from_file(filename, mime=True)
-        ext = mimetypes.guess_extension(mime)
-        return os.path.join(dst, f"index{ext}")
-
-    async def migrate_html_elements(self):
-        html_elements = migrate.HTMLElements(html_parser=self._html_parser,
-                                             http_downloader=self._http_downloader,
-                                             filepath_gen=self._filepath_gen,
-                                             url_resolver=self._resolver,
-                                             element_filter=HTMLElementFilters.element_filter)
-        await html_elements.migrate_dependencies()
-
-    async def migrate_css(self):
-        html_elements = migrate.HTMLElements(html_parser=self._html_parser,
-                                             http_downloader=self._http_downloader,
-                                             filepath_gen=self._filepath_gen,
-                                             url_resolver=self._resolver,
-                                             element_filter=HTMLElementFilters.link_filter)
-        await html_elements.migrate_dependencies()
-
-    async def migrate_iframes(self):
-        pass
-
-    async def migrate_js(self):
-        html_elements = migrate.HTMLElements(html_parser=self._html_parser,
-                                             http_downloader=self._http_downloader,
-                                             filepath_gen=self._filepath_gen,
-                                             url_resolver=self._resolver,
-                                             element_filter=HTMLElementFilters.js_filter)
-        await html_elements.migrate_dependencies()
-
-    def save_index(self):
-        with open(self._index_path, "wb") as fd:
-            fd.write(self._html_parser.export())
+    def __flatten(rules):
+        flatten_rules = [(name, attr) for name, attrs in rules.items() for attr in attrs]
+        return flatten_rules
