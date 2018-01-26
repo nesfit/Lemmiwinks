0bbca54e540e2e1414bf4a170463228a2bb3fe88
Viliam Serecun <v.serecun@gmail.com>
2018-01-26 16:31:06 +0100

    mod: rewrote old path generator to the new version
    I modified the code by using standard library 'pathlib'. This
    modification helped to have a much more clear implementation.
    
    Signed-off-by: Viliam Serecun <v.serecun@gmail.com>

---
+++ lemmiwinks/pathgen.py
@@ -0,0 +1,53 @@
+import pathlib
+import uuid
+import collections
+
+Path = collections.namedtuple('Path', 'abs rel')
+
+
+class DirectoryWrapper:
+    def __init__(self, location: str):
+        self._dirpath = pathlib.Path(location).resolve()
+        self._files = set()
+
+        if not self._dirpath.exists() or not self._dirpath.is_dir():
+            raise NotADirectoryError(
+                f"Path '{location}' doesn't exist or it is not a directory.")
+
+    def get_filepath_with(self, extension: str) -> pathlib.Path:
+        filepath = self.__generate_unique_filepath_with(extension)
+        self._files.add(filepath)
+        return filepath
+
+    def __generate_unique_filepath_with(self, extension: str) -> pathlib.Path:
+        filepath = self.__generate_filepath_with(extension)
+
+        while filepath in self._files or filepath.exists():
+            filepath = self.__generate_filepath_with(extension)
+
+        return filepath
+
+    def __generate_filepath_with(self, extension: str) -> pathlib.Path:
+        filename = f"{uuid.uuid4().hex}{extension}"
+        return self._dirpath.joinpath(filename)
+
+
+class FilePathGenerator:
+    def __init__(self, directory: object, path_prefix: str):
+        self._directory = directory
+        self.path_prefix = path_prefix
+
+    def generate_filepath_with(self, extension: str) -> Path:
+        abs_path = self.__generate_abs_filepath_with(extension)
+        rel_path = abs_path.relative_to(self.path_prefix)
+        return Path(abs=str(abs_path), rel=str(rel_path))
+
+    def __generate_abs_filepath_with(self, extension: str) -> pathlib.Path:
+        return self._directory.get_filepath_with(extension)
+
+
+class FilePathGeneratorContainer:
+    @classmethod
+    def filepath_generator(cls, location, path_prefix):
+        directory = DirectoryWrapper(location)
+        return FilePathGenerator(directory, path_prefix)
