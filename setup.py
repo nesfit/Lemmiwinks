1832765d6b96619c3f87cd26dd2c4e19a6996382
vserecun <v.serecun@gmail.com>
2018-03-18 16:44:15 +0100

    mod: settup.py added settings
    Signed-off-by: vserecun <v.serecun@gmail.com>

--- setup.py
+++ setup.py
@@ -1,2 +1,27 @@
-#!/usr/bin/env python3.6
+from setuptools import setup, find_packages
+from setuptools.command import build_py
 
+setup(name='lemmiwinks',
+      vesrion='0.0.1',
+      author='Viliam Serecun',
+      author_email='v.serecun@gmail.com',
+      packages=['lemmiwinks'],
+      licence="MIT",
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+      ],
+      install_requires=[
+          'tinycss2>=0.6.1',
+          'aiohttp>=2.3.7',
+          'aiofiles>=0.3.2',
+          'selenium>=3.8.1',
+          'dependency-injector>=3.9.1',
+          'validators>=0.12.0',
+          'python-magic>=0.4.15',
+          'beautifulsoup4>=4.6.0',
+          'asyncio-extras>=1.3.0',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
